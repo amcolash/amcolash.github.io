7c160606aca0acb2b4065891cf2641d8a92deec4
Andrew McOlash <amcolash@gmail.com>
2014-08-09 00:18:49 -0500

    First Commit

---
+++ app/router.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: HomeENV.locationType
+});
+
+Router.map(function() {
+  this.route("projects", { path: "/projects" });
+  this.route("about", { path: "/about" });
+});
+
+export default Router;
