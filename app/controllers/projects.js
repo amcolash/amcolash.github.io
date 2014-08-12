6dbf1fa6422f4aafe7fc8300845c1e99bd917d9c
Andrew McOlash <amcolash@gmail.com>
2014-08-11 23:22:35 -0500

    Minor tweaks, wording and sorting changed to last push from names

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  name : true,
-  push : false,
+  name : false,
+  push : true,
 
   actions : {
     nameSort : function() {
