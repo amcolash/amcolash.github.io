fc73e351b2cab7ce88ec678335a00871f927d775
Andrew McOlash <amcolash@gmail.com>
2014-08-20 23:49:43 -0400

    added error template

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route("projects", { path: "/projects" });
   this.route("about", { path: "/about" });
+  this.route("error", { path: "/error" });
 });
 
 export default Router;
