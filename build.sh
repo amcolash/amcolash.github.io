451cb6cd36b860d7c247410dbbabe2f11d8dd38a
Andrew McOlash <amcolash@gmail.com>
2018-03-25 17:57:58 -0500

    Slight ui tweaks to website

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Run new build of static content
-JEKYLL_ENV=production bundle exec jekyll build
-
 # Run new build of resume
 pushd resume
     ./index.js
 popd
 
-# Copy resume to output folder
-mkdir docs/resume
-cp -r resume/public/* docs/resume/
+# Run new build of static content
+JEKYLL_ENV=production bundle exec jekyll build
